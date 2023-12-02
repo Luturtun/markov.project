cb5ddf903a0e44cc976b84631a702ee8bc1b1c43
tugbatumer <45402554+tugbatumer@users.noreply.github.com>
2023-12-02 21:46:53 +0100

    add q3

--- helpers.py
+++ helpers.py
@@ -1,16 +1,22 @@
 import random
 import numpy as np
+import scipy
 
 
 def f_1_5(X, theta, y):
     return np.sum((y - np.dot(X, theta)) ** 2)
 
 
+def f_3(X, theta, y):
+    z = X @ theta / np.sqrt(2)  # Compute X_i^T * theta / sqrt(2)
+    log_likelihood = np.log(1 + y * scipy.special.erf(z) + 1e-7)  # Compute log-likelihood for each observation
+    neg_log_likelihood = -np.sum(log_likelihood)  # Sum up the negative log-likelihood values
+    return neg_log_likelihood
+
+
 def mh_1_5(X, theta, y, beta, num_iter):
     m = X.shape[0]
     counter = 0
-    avg_f_difference = 0
-    difference_counter = 0
     for i in range(num_iter):
         counter += 1
         if counter == 50000:
@@ -20,7 +26,6 @@ def mh_1_5(X, theta, y, beta, num_iter):
         theta_proposed[idx] = 1 - theta_proposed[idx]
         f_theta_proposed = f_1_5(X, theta_proposed, y)
         f_theta = f_1_5(X, theta, y)
-        avg_f_difference += f_theta_proposed - f_theta
 
         if f_theta_proposed < f_theta:
             theta = theta_proposed
@@ -30,12 +35,9 @@ def mh_1_5(X, theta, y, beta, num_iter):
             if acceptance_probability >= np.random.uniform(0, 1):
                 theta = theta_proposed
                 counter = 0
-            else:
-                avg_f_difference += f_theta_proposed - f_theta
-                difference_counter += 1
         if i%5000 == 0:
             print(f"m = {m}, beta = {beta}, Iteration #: {i}, f_theta = {f_theta}")
-    return theta, avg_f_difference/difference_counter
+    return theta
 
 
 def mh_2(X, theta, y, beta, num_iter):
@@ -67,6 +69,118 @@ def mh_2(X, theta, y, beta, num_iter):
     return theta
 
 
+def mh_3(X, theta, y, beta, num_iter):
+    m = X.shape[0]
+    counter = 0
+    for i in range(num_iter):
+        counter += 1
+        if counter == 50000:
+            break
+        zero_indices = np.where(theta == 0)[0]
+        one_indices = np.where(theta == 1)[0]
+        zero_index = np.random.choice(zero_indices)
+        one_index = np.random.choice(one_indices)
+        theta_proposed = theta.copy()
+        theta_proposed[zero_index] = 1
+        theta_proposed[one_index] = 0
+        f_theta_proposed = f_3(X, theta_proposed, y)
+        f_theta = f_3(X, theta, y)
+        if f_theta_proposed < f_theta:
+            theta = theta_proposed
+            counter = 0
+        else:
+            acceptance_probability = np.exp(-beta * (f_theta_proposed - f_theta))
+            if acceptance_probability >= np.random.uniform(0, 1):
+                theta = theta_proposed
+                counter = 0
+        if i%5000 == 0:
+            print(f"m = {m}, beta = {beta}, Iteration #: {i}, f_theta = {f_theta}")
+    return theta
+
+def simulated_annealing_mh_3(X, theta, y, beta_2m_start, beta_2m_end, seq_length, num_iter):
+
+    m = X.shape[0]
+    beta_start = beta_2m_start / (2 * m)
+    beta_end = beta_2m_end / (2 * m)
+
+    result_sequence = geometric_sequence(beta_start, beta_end, seq_length)
+    seq_length = len(result_sequence)
+
+    beta_index = 0
+    beta = result_sequence[beta_index]
+    counter = 0
+    for i in range(num_iter):
+        counter += 1
+        if counter == 50000:
+            break
+        if i != 0 and i % (int(num_iter/seq_length)) == 0:
+            beta_index += 1
+            beta = result_sequence[beta_index]
+        zero_indices = np.where(theta == 0)[0]
+        one_indices = np.where(theta == 1)[0]
+        zero_index = np.random.choice(zero_indices)
+        one_index = np.random.choice(one_indices)
+        theta_proposed = theta.copy()
+        theta_proposed[zero_index] = 1
+        theta_proposed[one_index] = 0
+        f_theta_proposed = f_3(X, theta_proposed, y)
+        f_theta = f_3(X, theta, y)
+
+        if f_theta_proposed < f_theta:
+            theta = theta_proposed
+            counter = 0
+        else:
+            acceptance_probability = np.exp(-beta * (f_theta_proposed - f_theta))
+            if acceptance_probability >= np.random.uniform(0, 1):
+                theta = theta_proposed
+                counter = 0
+        if i%5000 == 0:
+            print(f"m = {m}, beta = {beta}, Iteration #: {i}, f_theta = {f_theta}")
+    return theta
+
+
+def simulated_annealing_mh_2(X, theta, y, beta_2m_start, beta_2m_end, seq_length, num_iter):
+
+    m = X.shape[0]
+    beta_start = beta_2m_start / (2 * m)
+    beta_end = beta_2m_end / (2 * m)
+
+    result_sequence = geometric_sequence(beta_start, beta_end, seq_length)
+    seq_length = len(result_sequence)
+
+    beta_index = 0
+    beta = result_sequence[beta_index]
+    counter = 0
+    for i in range(num_iter):
+        counter += 1
+        if counter == 50000:
+            break
+        if i != 0 and i % (int(num_iter/seq_length)) == 0:
+            beta_index += 1
+            beta = result_sequence[beta_index]
+        zero_indices = np.where(theta == 0)[0]
+        one_indices = np.where(theta == 1)[0]
+        zero_index = np.random.choice(zero_indices)
+        one_index = np.random.choice(one_indices)
+        theta_proposed = theta.copy()
+        theta_proposed[zero_index] = 1
+        theta_proposed[one_index] = 0
+        f_theta_proposed = f_1_5(X, theta_proposed, y)
+        f_theta = f_1_5(X, theta, y)
+
+        if f_theta_proposed < f_theta:
+            theta = theta_proposed
+            counter = 0
+        else:
+            acceptance_probability = np.exp(-beta * (f_theta_proposed - f_theta))
+            if acceptance_probability >= np.random.uniform(0, 1):
+                theta = theta_proposed
+                counter = 0
+        if i%5000 == 0:
+            print(f"m = {m}, beta = {beta}, Iteration #: {i}, f_theta = {f_theta}")
+    return theta
+
+
 def simulated_annealing(X, theta, y, beta_2m_start, beta_2m_end, seq_length, num_iter):
 
     m = X.shape[0]
@@ -74,6 +188,7 @@ def simulated_annealing(X, theta, y, beta_2m_start, beta_2m_end, seq_length, num
     beta_end = beta_2m_end / (2 * m)
 
     result_sequence = geometric_sequence(beta_start, beta_end, seq_length)
+    seq_length = len(result_sequence)
 
     beta_index = 0
     beta = result_sequence[beta_index]
